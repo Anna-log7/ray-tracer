94ab5f9c9acfd10bc9473608bf30311005029180
Anna <adavies12@protonmail.com>
2020-08-01 19:34:55 -0700

    feat: add equality checks for tuples

--- internal/tuples/tuples.go
+++ internal/tuples/tuples.go
@@ -1,23 +1,58 @@
 package tuples
 
-type tuple struct {
-	x float32
-	y float32
-	z float32
-	w float32
+import (
+	"math"
+	"reflect"
+)
+
+// TupleStruct data type
+type TupleStruct struct {
+	X float64
+	Y float64
+	Z float64
+	W float64
 }
 
 // Tuple returns tuple
-func Tuple(x float32, y float32, z float32, w float32) tuple {
-	return tuple{x, y, z, w}
+func Tuple(x float64, y float64, z float64, w float64) TupleStruct {
+	return TupleStruct{x, y, z, w}
 }
 
 // Point returns tuple with w = 1
-func Point(x float32, y float32, z float32) tuple {
-	return Tuple(x, y, z, 1)
+func Point(x float64, y float64, z float64) TupleStruct {
+	return TupleStruct{x, y, z, 1}
 }
 
 // Vector returns tuple with w = 0
-func Vector(x float32, y float32, z float32) tuple {
-	return Tuple(x, y, z, 0)
+func Vector(x float64, y float64, z float64) TupleStruct {
+	return TupleStruct{x, y, z, 0}
+}
+
+// FloatsEqual checks if 2 floats are equal
+func FloatsEqual(val1 float64, val2 float64) bool {
+	EPSILLON := .00001
+	return math.Abs(val1-val2) < EPSILLON
+}
+
+// Equal checks if 2 tuples are equal
+func Equal(tup1 TupleStruct, tup2 TupleStruct) bool {
+	val1 := reflect.ValueOf(tup1)
+	typeOfVal1 := val1.Type()
+
+	for i := 0; i < val1.NumField(); i++ {
+		field := typeOfVal1.Field(i).Name
+		if !FloatsEqual(tup1[field], tup2[field]) {
+			return false
+		}
+	}
+	return true
+}
+
+func AddTuples(tup1 TupleStruct, tup2 TupleStruct) TupleStruct {
+	x := tup1.x + tup2.x
+	y := tup1.y + tup2.y
+	z := tup1.z + tup2.z
+	w := tup1.w + tup2.w
+
+	return Tuple(x, y, z, w)
 }
--- internal/tuples/tuples_test.go
+++ internal/tuples/tuples_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestTuple(t *testing.T) {
 	a := Tuple(4.3, -4.2, 3.1, 1.0)
-	if a.x != 4.3 || a.y != -4.2 || a.z != 3.1 || a.w != 1.0 {
+	if a.X != 4.3 || a.Y != -4.2 || a.Z != 3.1 || a.W != 1.0 {
 		t.Fail()
 	}
 }
@@ -21,6 +21,15 @@ func TestPoint(t *testing.T) {
 func TestVector(t *testing.T) {
 	v := Vector(4, -4, 3)
 	if v != Tuple(4, -4, 3, 0) {
+		t.Fail()
+	}
+}
+
+func TestAdditon(t *testing.T) {
+	tup1 := Tuple(3, -2, 5, 1)
+	tup2 := Tuple(-2, 3, 1, 0)
+
+	if AddTuples(tup1, tup2) != Tuple(1, 1, 6, 1) {
 		t.Fail()
 	}
 }
